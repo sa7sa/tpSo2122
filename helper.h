8bcfb1832cc61fa12c978ec659e0f302e53a015d
Salsa <maildojordao@gmail.com>
2021-10-29 23:31:34 +0100

    all

--- helper.h
+++ helper.h
@@ -3,15 +3,15 @@
 
 //Estrutura onde são guardados os dados dos clientes
 typedef struct cliente{
-	char nome[20];
-	char sintoma[];
+	char nome[20];			//Nome do cliente
+	char sintoma[];		//Qual o sintoma
 }doente;
 
 //Estrutura onde são guardados os dados devolvidos pelo balcão
 typedef struct avaliaCliente{
-	int prioridade;
-	int espera;
-	char expecialidade[20];
+	int prioridade;		//Prioridade do sintoma
+	int tempoEspera;		//Clientes em fila de espera
+	char especialidade[20];	//Médico da especialidade
 }AvDoente
 
 #endif //HELPER_H
