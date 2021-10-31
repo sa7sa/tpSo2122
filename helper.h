4711aa7655cc419a4a2a01a3e586b4b9eb03ef2e
Celso Jordão <maildojordao@gmail.com>
2021-10-31 14:52:00 +0000

    --all

--- helper.h
+++ helper.h
@@ -4,31 +4,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
+#include <ctype.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
-#include "funcoes.h"
-
 
 #define MAX_TAM 50
 #define FIFO_BALCAO "fBalcao"		//Nome do pipe do balcão
 #define FIFO_CLIENTE "fCliente_%d"	//Nome do pipe de cliente
 
 //Estrutura onde são guardados os dados dos clientes
-typedef struct cliente{
+typedef struct{
 	char nome[MAX_TAM];			//Nome do cliente
 	char sintoma[MAX_TAM];		//Qual o sintoma
 }doente;
 
 //Estrutura onde são guardados os dados devolvidos pelo balcão
-typedef struct avalia_cliente{
+typedef struct{
 	int prioridade;		//Prioridade do sintoma
 	int tempoEspera;		//Clientes em fila de espera
 	char espMed[MAX_TAM];	//Médico da especialidade
 }av_doente;
 
 //Estrutura com as definições dos médicos
-typedef struct especialista{
+typedef struct{
 	char nomeMedico[MAX_TAM];
 	char especialidade[MAX_TAM];
 }medico_esp;
